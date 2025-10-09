f1ee3e017de7b8c51544dde9364e683a3de876d9
code-Lance <hyunjune2001@gmail.com>
2025-10-09 20:15:38 +0900

    [Feat] 특정 나라 인기 노래 조회 툴 생성 1. 특정 나라 인기 노래 조회 툴 생성 - Agent에서 사용할 특정 나라 인기 노래를 찾아주는 툴 생성

--- app/agent/tools/__init__.py
+++ app/agent/tools/__init__.py
@@ -1,7 +1,7 @@
 from .guardrails import moderation_tool
 from .tag_tracks import TagTracks
 # from .artist_tracks import ArtistTracks
-# from .country_tracks import CountryTracks
+from .country_tracks import CountryTracks
 # from .global_tracks import GlobalTracks
 # from .similar_artists import SimilarArtists
 # from .similar_tracks import SimilarTracks
@@ -10,7 +10,7 @@ TOOLS = [
     moderation_tool,
     TagTracks.get_tracks_by_tag,
     # ArtistTracks.get_top_tracks_by_artist,
-    # CountryTracks.get_top_tracks_by_country,
+    CountryTracks.get_top_tracks_by_country,
     # GlobalTracks.get_global_top_tracks,
     # SimilarArtists.get_similar_artists,
     # SimilarTracks.get_similar_tracks,
